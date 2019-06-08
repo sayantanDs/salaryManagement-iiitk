69452efbf31c4fa12bd135a8b3d442282d922132
sayantanDs <sayantanapril99@gmail.com>
2019-06-08 11:23:26 +0530

    removed global variable db and added class Database which will contain a a static instance of DatabaseManager

--- DatabaseManager/__init__.py
+++ DatabaseManager/__init__.py
@@ -1,7 +1,3 @@
-from databaseManager import DatabaseManager
-
-# Global variable to store the database object
-# This will be initialized from the main with proper exception handling
-db = None
+from databaseManager import Database
 
 
