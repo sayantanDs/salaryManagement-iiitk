16f09712917f391d014e9d23d5e807ec8711cd34
sayantanDs <sayantanapril99@gmail.com>
2019-06-09 18:00:24 +0530

    added Designation class

---
+++ CustomClasses/Designation.py
@@ -0,0 +1,8 @@
+class Designation:
+    def __init__(self, designation, da, hra, ta, it, pt):
+        self.designation = designation
+        self.da = float(da)
+        self.hra = float(hra)
+        self.ta = float(ta)
+        self.it = float(it)
+        self.pt = float(pt)
