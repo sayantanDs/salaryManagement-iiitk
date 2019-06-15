e63220d9df72761c1f5e9d6dfd5e0ceb4a5434fa
sayantanDs <sayantanapril99@gmail.com>
2019-06-15 11:23:41 +0530

    improved ValidatingLineEdit - added tick/cross icon and tooltip on wrong input

--- CustomWidgets/validatingLineEdit.py
+++ CustomWidgets/validatingLineEdit.py
@@ -6,6 +6,7 @@ class ValidatingLineEdit(QtGui.QLineEdit):
     def __init__(self, fieldName, regex, parent=None):
         QtGui.QLineEdit.__init__(self, parent)
         self.fieldName = fieldName
+        self.__edited = False
 
         if isinstance(regex, QtGui.QValidator):
             # self.setValidator(regex)
@@ -18,20 +19,41 @@ class ValidatingLineEdit(QtGui.QLineEdit):
 
         self.textChanged.connect(self.__adjustColor)
 
+        self.button = QtGui.QToolButton(self)
+        self.button.setStyleSheet("QToolButton{background: transparent;}")
+        self.button.setIconSize(QtCore.QSize(18, 18))
+
+    def resizeEvent(self, *args, **kwargs):
+        QtGui.QLineEdit.resizeEvent(self, *args, **kwargs)
+        buttonSize = self.button.sizeHint()
+        self.button.move(self.rect().right() - buttonSize.width(),
+                         (self.rect().bottom() - buttonSize.height() + 1) / 2)
+
     def __adjustColor(self):
-        # if self.hasAcceptableInput():
+        self.__edited = True
         if self.isValid():
             self.setObjectName("valid")
+            self.button.setIcon(QtGui.QIcon(QtGui.QPixmap("Resources/icons8-ok-48.png")))
         else:
             self.setObjectName("invalid")
+            self.button.setIcon(QtGui.QIcon(QtGui.QPixmap("Resources/icons8-cancel-48.png")))
         self.setStyleSheet(self.styleSheet())
+
+    def __toDefaultColor(self):
+        self.setObjectName("default")
+        self.button.setIcon(QtGui.QIcon())
+        self.__edited = False
 
     def isValid(self):
         state, _, _ = self.__validator.validate(self.text(), len(self.text()))
         if state == QtGui.QValidator.Acceptable:
             return True
         return False
-        # return self.hasAcceptableInput()
+
+    def enterEvent(self, *args, **kwargs):
+        QtGui.QLineEdit.enterEvent(self, *args, **kwargs)
+        if self.__edited and not self.isValid():
+            QtGui.QToolTip.showText(self.mapToGlobal(self.rect().topRight()), self.getErrorMessage(), self)
 
     def getErrorMessage(self):
         if self.isValid():
@@ -43,8 +65,8 @@ class ValidatingLineEdit(QtGui.QLineEdit):
 
     def setText(self, *args, **kwargs):
         QtGui.QLineEdit.setText(self, *args, **kwargs)
-        self.setObjectName("default")
+        self.__toDefaultColor()
 
     def clear(self, *args, **kwargs):
         QtGui.QLineEdit.clear(self, *args, **kwargs)
-        self.setObjectName("default")
+        self.__toDefaultColor()
